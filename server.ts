28f9e8232f07f27a3ecfa2e49682022d492a2690
ikcilrep <frajerzycki@protonmail.com>
2020-09-05 13:15:24 +0200

    Add router

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./router/mod.ts";
 
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello world!";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
